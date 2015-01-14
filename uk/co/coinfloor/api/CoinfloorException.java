1abe08e8b1da3bcffdec3a0bf9006fd83c787c10
Coinfloor LTD <developer@coinfloor.co.uk>
2015-01-14 04:40:57 -0500

    remove redundant field in CoinfloorException

--- uk/co/coinfloor/api/CoinfloorException.java
+++ uk/co/coinfloor/api/CoinfloorException.java
@@ -5,12 +5,10 @@ public class CoinfloorException extends Exception {
 	private static final long serialVersionUID = 0L;
 
 	private final int errorCode;
-	private final String errorMessage;
 
 	public CoinfloorException(int errorCode, String errorMessage) {
 		super(errorMessage);
 		this.errorCode = errorCode;
-		this.errorMessage = errorMessage;
 	}
 
 	public int getErrorCode() {
@@ -18,11 +16,12 @@ public class CoinfloorException extends Exception {
 	}
 
 	public String getErrorMessage() {
-		return errorMessage;
+		return super.getMessage();
 	}
 
 	@Override
 	public String getMessage() {
+		String errorMessage = super.getMessage();
 		return errorMessage == null ? '<' + String.valueOf(errorCode) + '>' : '<' + String.valueOf(errorCode) + "> " + errorMessage;
 	}
 
